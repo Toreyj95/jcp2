ba3d0802b1d4157810726e53897a1b81ee4f3acf
Toreyj95 <164526917+Toreyj95@users.noreply.github.com>
2024-05-01 23:32:12 -0400

    jcp2

---
+++ jcp2.js
@@ -0,0 +1,16 @@
+const friend = "BRUTUS";
+const shiftValue = 3;
+const alphabet = abcdefghijklmnopqrstuvwxyz;
+const firstLetter = friend[0];
+const index = alphabet.indexOf(firstLetter.toLocaleLowerCase());
+const newIndex = index + shiftValue;
+const encryptedFirstLetter = alphabet[newIndex];
+const alphabetLength = alphabet.length;
+const NewIndex = (index + shiftValue) % alphabetLength;
+const EncryptedFirstLetter = alphabet[NewIndex];
+const encryptedMessage = "EUXWXV"
+const teaserMessage = encryptedMessage.slice(0,3);
+const randomDecimal = Math.random()
+const 
+
+
